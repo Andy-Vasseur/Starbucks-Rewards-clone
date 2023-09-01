54c21892933a1b55ef0afbd70bbf309aceb8ab41
Andy-Vasseur <beaugosswwe@live.fr>
2023-09-01 17:09:11 +0200

    .

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,15 @@
 function Hero() {
     return (
         <div className="Hero">
-            <h1>dsdsds</h1>
+            <h1>Free Coffee is a tap away</h1>
+            <p>Join now to start earning Rewards.</p>
+            <a href="#">
+                Join now
+            </a>
+            <p>Or
+                <span> join in the app </span>
+                for the best experience
+            </p>
         </div>
     )
 }
